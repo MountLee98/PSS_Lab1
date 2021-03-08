088772a3417540a2428ac0ae247d08571c808fed
MountLee98 <michalpa98@gmail.com>
2021-03-08 22:59:21 +0100

    add PSS_Lab1 project

---
+++ src/main/java/lab/pai/PaiApplication.java
@@ -0,0 +1,13 @@
+package lab.pai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaiApplication.class, args);
+	}
+
+}
